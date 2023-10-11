afb13dd9ba104b6d9bb3b8c04e1742617915a394
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2023-10-11 20:52:22 -0300

    Y barra corrigida

--- Parte 1/script_principal.m
+++ Parte 1/script_principal.m
@@ -1,4 +1,4 @@
-%Última edição: Ricardo - 2023/10/04 00:09:
+%Última edição: Ricardo - 2023/10/11 20:33:
 clear
 NA = 1;
 
@@ -11,8 +11,8 @@ Zb1 = Vb1^2/Sb;
 Zb2 = Vb2^2/Sb;
 Zb3 = Vb3^2/Sb;
 Ib1 = Sb/(sqrt(3)*Vb1);
-Ib2 = Sb/(sqrt(3)*Vb1);
-Ib3 = Sb/(sqrt(3)*Vb1);
+Ib2 = Sb/(sqrt(3)*Vb2);
+Ib3 = Sb/(sqrt(3)*Vb3);
 
 %Impedâncias do circuito em pu.
 Z1_lt02c1 = (0.08953 + 0.53092j) * (180+NA)/ Zb1;
@@ -23,15 +23,15 @@ Z1_lt03c1 = (0.06731 + 0.48932j) * (480+NA) / Zb1;
 Z1_lt04c1 = (0.03752 + 0.50121j) * (550+NA/10) / Zb1;
 Z1_lt04c2 = (0.03752 + 0.50121j) * (550+NA/10) / Zb1;
 Z1_lt01j1 = (0.02898 + 0.47598j) * (45+NA/10) / Zb2;
-Z1_lt02j1 = (0.02898 + 0.47598j) * (35+NA/10) / Zb2 ;
+Z1_lt02j1 = (0.02898 + 0.47598j) * (35+NA/10) / Zb2;
 Z1_lt03j1 = (0.04852 + 0.58921j) * (21+NA/10) / Zb2;
 Z1_lt04j1 = (0.02898 + 0.47598j) * (25+NA/10) / Zb2;
 Z1_lt01k1 = (0.02682 + 0.49327j) * (4+NA/10) / Zb3;
 Zat_t1 = 1j*(6.2 + NA*1e-2) * 100/15;
 %Impedâncias TR01T1 - Yd1d1
-Zps = (0.0481 + NA*1e-4) + (7.5457 + NA*1e-2)*1j;
-Zpt = (0.0991 + NA*1e-4) + (16.7381 + NA*1e-2)*1j;
-Zst = (0.0398 + NA*1e-4) + (4.9875 + NA*1e-2)*1j;
+Zps = ((0.0481 + NA*1e-4) + (7.5457 + NA*1e-2)*1j)/100;
+Zpt = ((0.0991 + NA*1e-4) + (16.7381 + NA*1e-2)*1j)/100;
+Zst = ((0.0398 + NA*1e-4) + (4.9875 + NA*1e-2)*1j)/100;
 Zst = Zst * 100/35; % Mudança de base
 Z_tr01t1 = [1 1 0; 1 0 1; 0 1 1]^-1 * [Zps; Zpt; Zst];
 Zp_tr01t1 = Z_tr01t1(1);
@@ -45,15 +45,65 @@ Z_tr02t1 = Z_tr02t1 * 100/20; % Mudança de base
 Z_tr03t1 = 1j*(5.5 + NA/100) * 1e-2;
 Z_tr03t1 = Z_tr03t1 * 100/30; % Mudança de base
 %Impedâncias equivalente de rede.
-Scc3fase = pol(14533.4986, 87.1794)*1e6;
-Zeq_barra1 = Vb1^2/conj(Scc3fase) * 1/Zb1;
-Zeq_barra3 = pol(13.5671, 86.8264)/Zb1
+Scc3fase = pol(14533.4986, 87.1794) * 1e6/100e6;
+Zeq_barra3 = 1/conj(Scc3fase);
+Zeq_barra1 = pol(13.5671, 86.8264) / 100;
 
 %Matriz Ybarra
 Y = zeros(11,11);
 Y(1,1) = 1/Zeq_barra1 + 1/Z1_lt02c1 + 1/Z1_lt02c2 + 1/Z1_lt04c1 + 1/Z1_lt04c2;
 Y(2,2) = 1/Z1_lt02c1 + 1/Z1_lt02c2 + 1/Z1_lt03c1;
+Y(3,3) = 1/Z1_lt01c2 + 1/Z1_lt01c1 + 1/Zeq_barra3;
+Y(4,4) = 1/Z1_lt01c2 + 1/Z1_lt01c1 + 1/Zps + 1/Z_tr01t2 +1/Z1_lt03c1;
+Y(5,5) = 1/Zps + 1/Z_tr01t2 + 1/Z1_lt01j1 + 1/Z1_lt02j1;
+Y(6,6) = 1/Z1_lt02j1 +  1/Z1_lt04j1 + 1/Z1_lt03j1;
+Y(7,7) = 1/Z1_lt03j1 + 1/Z_tr03t1;
+Y(8,8) = 1/Z_tr03t1 + 1/Z1_lt04c1 + 1/Z1_lt04c2;
+Y(9,9) = 1/Z1_lt04j1 + 1/Z1_lt01j1 + 1/Z_tr02t1;
+Y(10,10) = 1/Z_tr02t1 + 1/Z1_lt01k1;
+Y(11,11) = 1/Z1_lt01k1;
+
+Y(1,2) = -(1/Z1_lt02c1 + 1/Z1_lt02c2);
+Y(2,1) = Y(1,2);
+
+Y(1,8) = -(1/Z1_lt04c1 + 1/Z1_lt04c2);
+Y(8,1) = Y(1,8);
+
+Y(2,4) = -1/Z1_lt03c1;
+Y(4,2) = Y(2,4);
+
+Y(3,4) = -(1/Z1_lt01c2 + 1/Z1_lt01c1);
+Y(4,3) = Y(3,4);
+
+Y(4,5) = -(1/Zps + 1/Z_tr01t2);
+Y(5,4) = Y(4,5);
+
+Y(5,6) = -1/Z1_lt02j1;
+Y(6,5) = Y(5,6);
+
+Y(5,9) = -1/Z1_lt01j1;
+Y(9,5) = Y(5,9);
+
+Y(6,9) = -1/Z1_lt04j1;
+Y(9,6) = Y(6,9);
+
+Y(6,7) = -1/Z1_lt03j1;
+Y(7,6) = Y(6,7);
+
+Y(7,8) = -1/Z_tr03t1;
+Y(8,7) = Y(7,8);
+
+Y(9,10) = -1/Z_tr02t1;
+Y(10,9) = Y(9,10);
+
+Y(10,11) = -1/Z1_lt01k1;
+Y(11,10) = Y(10,11);
+
+issymmetric(Y)
+%Matriz Zbarra
+Z = Y^-1;
+
+
 %Falta concluir Y barra
 
-%Matriz Zbarra
-%Z = Y^-1
+
