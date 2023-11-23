03980300e392adc4e97659fc4e7c603df266f744
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2023-11-23 18:19:59 -0300

    Correntes Circunvizinhas

--- Parte 2/Scripts/printCorrente012.m
+++ Parte 2/Scripts/printCorrente012.m
@@ -1,19 +1,19 @@
-function printCorrente(complexo,base)
+function printCorrente012(complexo,base)
     % Printa magnitude e angulo em graus de um vetor de números complexos.
     % Função printPolar modificada.
-    % Printa três fases da corrente em pu e Amperes.
+    % Printa três sequências da corrente em pu e Amperes.
     %tipo - Representa se quer printar
     magnitude = abs(complexo);
     angulo = rad2deg(angle(complexo));
-    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    seqs = ["Zero","Positiva","Negativa"];
     fprintf("Valores em pu:\n")
     for i = 1:length(complexo)
-        fase_rotulo = sprintf('Fase %s:', alfabeto(i));
+        fase_rotulo = sprintf('Seq %s:', seqs(i));
         fprintf('%s %.3f ∠ %.3f° pu\n', fase_rotulo, magnitude(i), angulo(i));
     end
     fprintf("Valores em Amperes:\n")
     for i = 1:length(complexo)
-        fase_rotulo = sprintf('Fase %s:', alfabeto(i));
+        fase_rotulo = sprintf('Seq %s:', seqs(i));
         fprintf('%s %.2f ∠ %.3f° A\n', fase_rotulo, magnitude(i)*base, angulo(i));
     end
 end
